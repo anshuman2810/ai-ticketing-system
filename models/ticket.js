08f61a625e3e318be17114d5995836fb2afeb21c
anshumanprajapati2810 <22it003@charusat.edu.in>
2025-07-19 22:27:57 +0530

    first commit

---
+++ models/ticket.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose"
+import user from "./user"
+
+const tickerSchema = new mongoose.Schema({
+    title : {type : String},
+    description : {type : String},
+    status : {type : String, defaule : "TODO"},
+    createdBy : {type : mongoose.Schema.Types.ObjectId, ref : "User"},
+    assignedTo : {type : mongoose.Schema.Types.ObjectId, ref : "User", default : null},
+    priotity : {type : String},
+    deadline : {type : Date},
+    helpfulNotes : {type : String},
+    relatedSkills : {type : [String]},
+    createdAt : {type : Date, default : Date.now()}
+})
+
+export default mongoose.model("Ticket", tickerSchema)
